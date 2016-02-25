b3487f8ae5679cb3039ae9366d2bab3bc4ee201c
Gordon Alexander MacPherson <gordon.a.macpherson@gmail.com>
2016-02-25 23:33:34 +0000

    Added textures and a script for rotation, which requires zero modification of the code, it can be used on cameras or rotors, probably many other things too.

---
+++ Assets/rotortest.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Rotor test script
+/// </summary>
+public class rotortest : MonoBehaviour {
+    public Vector3 RotationAxis = Vector3.zero;
+    public float speed;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate((RotationAxis * speed) * Time.deltaTime);
+	}
+}
