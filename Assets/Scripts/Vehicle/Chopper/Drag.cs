e45c2f89f5e152885a304cec4b575a093c1fef11
RevoluPowered <gordon.a.macpherson@gmail.com>
2016-04-05 19:03:18 +0100

    Minor updates to drag and scene.

--- Assets/Scripts/Vehicle/Chopper/Drag.cs
+++ Assets/Scripts/Vehicle/Chopper/Drag.cs
@@ -3,11 +3,17 @@ using System.Collections;
 
 [RequireComponent(typeof(Rigidbody))]
 public class Drag : MonoBehaviour {
+
+    public float 
+
     private Rigidbody mRigidbody;
 	// Use this for initialization
 	void Start () {
         mRigidbody = GetComponent<Rigidbody>();
-        mRigidbody.drag = 0.15555566f; // Meh.
+        mRigidbody.drag = 0.15555566f; 
+
+        //
+
 	}
 	
 	// Update is called once per frame
