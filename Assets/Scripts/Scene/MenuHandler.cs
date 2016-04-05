0ac84c1ab92b00b79b67bc005785a2199a726772
RevoluPowered <gordon.a.macpherson@gmail.com>
2016-04-05 12:58:15 +0100

    Added working menu and game seems operational.
    Signed-off-by: RevoluPowered <gordon.a.macpherson@gmail.com>

---
+++ Assets/Scripts/Scene/MenuHandler.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using System;
+/// <summary>
+/// Unity Menu Handler / Scene Handling.
+/// </summary>
+public class MenuHandler : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            //Console.Log("Escape key pressed returning to main menu");
+            MainMenu();
+        }
+	}
+
+    public void StartGame()
+    {
+       // Console.Log("Loading staging scene.");
+        SceneManager.LoadScene("Scenes/Main level - Staging");
+    }
+
+    public void MainMenu()
+    {
+      //  Console.Log("Loading Main Menu scene.");
+        SceneManager.LoadScene("Scenes/Main Menu");
+    }
+
+    public void ExitGame()
+    {
+      //  Console.Log("Exiting the game");
+        Application.Quit();
+    }
+}
